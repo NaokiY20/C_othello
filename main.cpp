6e97cf1e8b5598928db84cf8f1cbd2aba0370f58
NaokiY20 <naokigsyoshida@gmail.com>
2021-03-06 15:59:39 +0900

    startUP

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #include "othello.hpp"
 
-int main(){
+void start_up(){
     setlocale(LC_ALL,"");
     initscr();
     set_color();
+}
 
-    Board b;
+int main(){
+    start_up();
     
+    Board b;
     b.print_board(5,5);
     getch();
     endwin();
